f212911f9e721ad5ca98e36e77bda2510e931511
Buddhika Madusanka <buddhikadevelopmetn@gmail.com>
2025-06-03 14:33:09 +0530

    Setup website responsive

--- src/components/ui/EmailConnectButton.tsx
+++ src/components/ui/EmailConnectButton.tsx
@@ -11,7 +11,7 @@ const EmailConnectButton = () => {
 
   return (
     <div>
-        <button onClick={sendEmail} className='bg-green-600/70 border-[1px] border-dashed border-green-500 cursor-pointer duration-200 hover:bg-green-600/50 px-8 py-3 rounded-xl font-bold flex items-center gap-2'>
+        <button onClick={sendEmail} className='bg-green-600/70 border-[1px] border-dashed border-green-500 cursor-pointer duration-200 hover:bg-green-600/50 px-8 h-[40px] text-[16px] rounded-xl font-bold flex items-center gap-2 '>
             Send Email
             <MdOutlineEmail className='text-[22px]' />
         </button>
